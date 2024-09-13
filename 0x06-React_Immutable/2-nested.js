5884a73f7c7c9434b8fa039cb3c4804723e82c89
Bendamzi <okunsanyabenjamin40@gmail.com>
2024-09-13 05:57:59 +0100

    My commit

---
+++ 0x06-React_Immutable/2-nested.js
@@ -0,0 +1,8 @@
+
+import { fromJS } from './node_modules/immutable/dist/immutable';
+
+export default function accessImmutableObject(object, array) {
+  const mappedObject = fromJS(object);
+
+  return mappedObject.getIn(array, undefined);
+}
